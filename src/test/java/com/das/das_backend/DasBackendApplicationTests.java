98815a0d968d8fc1143f5a972bb1c799e62ce408
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-07 22:17:29 +0900

    ⚙ :: Project Setting

---
+++ src/test/java/com/das/das_backend/DasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.das.das_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
