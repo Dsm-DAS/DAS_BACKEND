58471df224ba988520f745ec5e87fae85983a750
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-13 11:39:57 +0900

    🗂 :: User 엔티티 생성

---
+++ src/main/java/com/das/das_backend/infrastructure/s3/DefaultImage.java
@@ -0,0 +1,11 @@
+package com.das.das_backend.infrastructure.s3;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class DefaultImage {
+
+    public static final String USER_PROFILE_IMAGE = "a";
+
+}
