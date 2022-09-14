d92ea2844eaae212e9d331d384b6224d297ffab7
김철우 <kimchul8747@daum.net>
2022-09-14 22:20:43 +0900

    🗂 :: like 엔티티 생성

---
+++ src/main/java/com/das/das_backend/domain/like/domain/Like.java
@@ -0,0 +1,37 @@
+package com.das.das_backend.domain.like.domain;
+
+
+import com.das.das_backend.domain.feed.domain.Feed;
+import com.das.das_backend.domain.user.domain.User;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Table(name = "tbl_feed_like")
+@Entity
+public class Like {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "feed_id")
+    private Feed feed;
+
+    @Builder
+    public Like(User user, Feed feed){
+        this.user = user;
+        this.feed = feed;
+    }
+
+}
