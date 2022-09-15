c1e8dadabcc23d6a335e0146b76440f9580c1087
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-15 21:32:37 +0900

    🐛 :: 엔티티 기본키 버그 수정

--- src/main/java/com/das/das_backend/domain/comment/domain/Comment.java
+++ src/main/java/com/das/das_backend/domain/comment/domain/Comment.java
@@ -2,24 +2,23 @@ package com.das.das_backend.domain.comment.domain;
 
 import com.das.das_backend.domain.feed.domain.Feed;
 import com.das.das_backend.domain.user.domain.User;
-import com.das.das_backend.global.entity.BaseTimeEntity;
+import com.das.das_backend.global.entity.BaseTimeIdEntity;
 import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Entity
-public class Comment extends BaseTimeEntity {
-
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+public class Comment extends BaseTimeIdEntity {
 
     @NotNull
     @Size(max = 500)
