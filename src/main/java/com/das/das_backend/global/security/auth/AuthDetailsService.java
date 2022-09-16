1605499698d78dc2f749b9541ce827e1e821eb3d
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-17 00:46:32 +0900

    🗂 :: AuthDetails, AuthDetailsService 추가

---
+++ src/main/java/com/das/das_backend/global/security/auth/AuthDetailsService.java
@@ -0,0 +1,24 @@
+package com.das.das_backend.global.security.auth;
+
+import com.das.das_backend.domain.user.domain.repository.UserRepository;
+import com.das.das_backend.domain.user.exception.UserNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@RequiredArgsConstructor
+@Service
+public class AuthDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String accountId) throws UsernameNotFoundException {
+        return userRepository.findByAccountId(accountId)
+                .map(AuthDetails::new)
+                .orElseThrow(() -> UserNotFoundException.EXCEPTION);
+    }
+
+}
