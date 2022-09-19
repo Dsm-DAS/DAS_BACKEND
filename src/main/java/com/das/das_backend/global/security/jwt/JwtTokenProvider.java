a226dcc0e46d515e9d0d579d56d9be05f7b96623
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-19 22:42:47 +0900

    🗂 :: JwtTokenProvider

---
+++ src/main/java/com/das/das_backend/global/security/jwt/JwtTokenProvider.java
@@ -0,0 +1,89 @@
+package com.das.das_backend.global.security.jwt;
+
+import com.das.das_backend.domain.auth.domain.RefreshToken;
+import com.das.das_backend.domain.auth.domain.repository.RefreshTokenRepository;
+import com.das.das_backend.global.exception.ExpiredTokenException;
+import com.das.das_backend.global.exception.InvalidTokenException;
+import com.das.das_backend.global.security.auth.AuthDetailsService;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.ExpiredJwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Date;
+
+@RequiredArgsConstructor
+@Component
+public class JwtTokenProvider {
+
+    private final JwtProperties jwtProperties;
+    private final AuthDetailsService authDetailsService;
+    private final RefreshTokenRepository refreshTokenRepository;
+
+    public String generateAccessToken(String id) {
+        return generateToken(id, "access", jwtProperties.getAccessExp());
+    }
+
+    public String generateRefreshToken(String id) {
+        String refreshToken = generateToken(id, "refresh", jwtProperties.getRefreshExp());
+        refreshTokenRepository.save(RefreshToken.builder()
+                .accountId(id)
+                .token(refreshToken)
+                .ttl(jwtProperties.getRefreshExp())
+                .build());
+
+        return refreshToken;
+    }
+
+    private String generateToken(String id, String type, Long exp) {
+        return Jwts.builder()
+                .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecretKey())
+                .setSubject(id)
+                .claim("type", type)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + exp * 1000))
+                .compact();
+    }
+
+    public String resolveToken(HttpServletRequest request) {
+        String bearerToken = request.getHeader(jwtProperties.getHeader());
+        return parseToken(bearerToken);
+    }
+
+    public Authentication authentication(String token) {
+        UserDetails userDetails = authDetailsService.loadUserByUsername(getBody(token).getSubject());
+        return new UsernamePasswordAuthenticationToken(userDetails, "", userDetails.getAuthorities());
+    }
+
+    public String parseToken(String bearerToken) {
+        if(bearerToken != null && bearerToken.startsWith(jwtProperties.getPrefix())
+                && bearerToken.length() > 7)
+            return bearerToken.substring(7);
+
+        return null;
+    }
+
+    public boolean isRefresh(String token) {
+        return getBody(token).get("type").equals("refresh");
+    }
+
+    private Claims getBody(String token) {
+        try {
+            return Jwts.parser()
+                    .setSigningKey(jwtProperties.getSecretKey())
+                    .parseClaimsJws(token)
+                    .getBody();
+        } catch (ExpiredJwtException e) {
+            throw ExpiredTokenException.EXCEPTION;
+        } catch (Exception e) {
+            throw InvalidTokenException.EXCEPTION;
+        }
+    }
+
+}
