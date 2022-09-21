7ed8314212035696842251488d6c59ae8f64f8f7
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-21 21:25:19 +0900

    🗂 :: 유저 회원가입 기능

---
+++ src/main/java/com/das/das_backend/domain/user/presentation/UserController.java
@@ -0,0 +1,25 @@
+package com.das.das_backend.domain.user.presentation;
+
+import com.das.das_backend.domain.user.presentation.dto.request.UserSignUpRequest;
+import com.das.das_backend.domain.user.service.UserSignUpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RequiredArgsConstructor
+@RequestMapping("/users")
+@RestController
+public class UserController {
+
+    private final UserSignUpService userSignUpService;
+
+    @PostMapping("/signup")
+    public void signup(@RequestBody @Valid UserSignUpRequest request) {
+        userSignUpService.execute(request);
+    }
+
+}
