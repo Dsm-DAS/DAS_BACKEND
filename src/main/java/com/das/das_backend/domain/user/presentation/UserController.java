a5e56b5ca0fd2650a8be7663765ee64c94b4c064
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-21 22:29:31 +0900

    ♻ :: Conflict 수정

--- src/main/java/com/das/das_backend/domain/user/presentation/UserController.java
+++ src/main/java/com/das/das_backend/domain/user/presentation/UserController.java
@@ -1,28 +1,27 @@
 package com.das.das_backend.domain.user.presentation;
 
-
+import lombok.RequiredArgsConstructor;
+import javax.validation.Valid;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.das.das_backend.domain.user.presentation.dto.request.UserSignUpRequest;
 import com.das.das_backend.domain.user.service.UserSignUpService;
-import lombok.RequiredArgsConstructor;
-import javax.validation.Valid;
 
-@RequiredArgsConstructor
-@RequestMapping("/users")
 @RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
 public class UserController {
-
-    
-    private final UserSignUpService userSignUpService;
-
-
-
 
     @PostMapping("/signup")
     public void signup(@RequestBody @Valid UserSignUpRequest request) {
         userSignUpService.execute(request);
     }
+
+    private final UserSignUpService userSignUpService;
+
+
+
+
 }
