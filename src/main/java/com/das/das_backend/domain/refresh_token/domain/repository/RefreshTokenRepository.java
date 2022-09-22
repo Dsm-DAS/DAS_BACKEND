ecf90654850c35f33677f249a1ddbfb2ef5a48f3
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-22 10:01:42 +0900

    🗂 :: updateToken 추가

--- src/main/java/com/das/das_backend/domain/refresh_token/domain/repository/RefreshTokenRepository.java
+++ src/main/java/com/das/das_backend/domain/refresh_token/domain/repository/RefreshTokenRepository.java
@@ -3,5 +3,10 @@ package com.das.das_backend.domain.refresh_token.domain.repository;
 import com.das.das_backend.domain.refresh_token.domain.RefreshToken;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+    Optional<RefreshToken> findByToken(String refreshToken);
+
 }
