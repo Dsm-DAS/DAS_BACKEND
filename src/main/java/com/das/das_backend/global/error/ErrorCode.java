7f7a85d1945dd513c900126fefd909d215fe086f
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-23 16:04:01 +0900

    🗂 :: WrongEmailException

--- src/main/java/com/das/das_backend/global/error/ErrorCode.java
+++ src/main/java/com/das/das_backend/global/error/ErrorCode.java
@@ -7,12 +7,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum ErrorCode {
 
+    WRONG_EMAIL(400, "Wrong Email"),
+
     INVALID_TOKEN(401, "Invalid Token"),
     INVALID_REFRESH_TOKEN(401, "Invalid Refresh Token"),
     EXPIRED_TOKEN(401, "Expired_Token"),
     PASSWORD_MIS_MATCH(401, "Password Mis Match"),
+
     FEED_CANNOT_UPDATE(401, "Feed Cannot Update"),
     FEED_CANNOT_DELETE(401, "Feed Cannot Delete"),
+    INVALID_AUTH_CODE(401, "Invalid Auth Code"),
+    UNVERIFIED_AUTH_CODE(401, "UnVerified Auth Code"),
 
     USER_NOT_FOUND(404, "User Not Found"),
     FEED_NOT_FOUND(404, "Feed Not Found"),
