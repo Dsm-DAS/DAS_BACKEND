81e3b819c54c7564ff6b500f8eca6a44ab49f356
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-09-25 00:28:14 +0900

    🗂 :: Property 추가

---
+++ src/main/java/com/das/das_backend/global/socket/property/SocketProperty.java
@@ -0,0 +1,11 @@
+package com.das.das_backend.global.socket.property;
+
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public class SocketProperty {
+
+    public static final String ERROR = "error";
+
+}
