56c243f10884e3211e3d0791b3bccb8b493c2efa
alsdl0629 <alsdl0629@dsm.hs.kr>
2022-09-28 12:01:57 +0900

    🗂 :: JmsProperties

---
+++ src/main/java/com/das/das_backend/global/util/jms/JmsProperties.java
@@ -0,0 +1,18 @@
+package com.das.das_backend.global.util.jms;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.boot.context.properties.ConstructorBinding;
+
+@Getter
+@AllArgsConstructor
+@ConstructorBinding
+@ConfigurationProperties(prefix = "spring.mail")
+public class JmsProperties {
+
+    private final String username;
+    private final Integer authExp;
+    private final String suffix;
+
+}
