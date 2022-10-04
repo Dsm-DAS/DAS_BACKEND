3ad8123226b6ae1fbb312017023de42cf45df163
KangMin <alsdl0629@dsm.hs.kr>
2022-10-04 20:00:57 +0900

    🌧 :: 댓글 가져오기 기능

--- src/main/java/com/das/das_backend/domain/comment/facade/CommentFacade.java
+++ src/main/java/com/das/das_backend/domain/comment/facade/CommentFacade.java
@@ -3,8 +3,15 @@ package com.das.das_backend.domain.comment.facade;
 import com.das.das_backend.domain.comment.domain.Comment;
 import com.das.das_backend.domain.comment.domain.repository.CommentRepository;
 import com.das.das_backend.domain.comment.exception.CommentNotFoundException;
+import com.das.das_backend.domain.feed.domain.Feed;
+import com.das.das_backend.domain.feed.presentation.dto.response.FeedDetailResponse;
+import com.das.das_backend.domain.feed.presentation.dto.response.FeedDetailResponse.CommentResponse;
+import com.das.das_backend.domain.feed.presentation.dto.response.FeedDetailResponse.Writer;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Component
@@ -15,6 +22,23 @@ public class CommentFacade {
     public Comment getComment(Integer commentId) {
         return commentRepository.findById(commentId)
                 .orElseThrow(() -> CommentNotFoundException.EXCEPTION);
+    }
+
+    public List<CommentResponse> getComments(Feed feed) {
+
+        return feed.getCommentList()
+                .stream().map(comment -> FeedDetailResponse.CommentResponse.builder()
+                        .commentId(comment.getId())
+                        .content(comment.getContent())
+                        .createdAt(comment.getCreatedAt())
+                        .updatedAt(comment.getUpdatedAt())
+                        .writer(Writer.builder()
+                                .userId(comment.getUser().getId())
+                                .name(comment.getUser().getName())
+                                .profileImageUrl(comment.getUser().getProfileImageUrl())
+                                .build())
+                        .build())
+                .collect(Collectors.toList());
     }
 
 }
