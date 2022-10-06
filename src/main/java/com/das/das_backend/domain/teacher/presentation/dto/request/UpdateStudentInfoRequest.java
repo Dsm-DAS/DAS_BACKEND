a9b3b5887eb23f6c19cb067f818c84e6a7cb66eb
jeong ho <dlwjdgh1209@dsm.hs.kr>
2022-10-06 15:34:47 +0900

    🌧 :: 학생 정보 수정해주기 API

---
+++ src/main/java/com/das/das_backend/domain/teacher/presentation/dto/request/UpdateStudentInfoRequest.java
@@ -0,0 +1,30 @@
+package com.das.das_backend.domain.teacher.presentation.dto.request;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@NoArgsConstructor
+public class UpdateStudentInfoRequest {
+
+    @NotBlank
+    @Size(min = 2, max = 10)
+    private String name;
+
+    @NotNull
+    private Integer userId;
+
+    @NotNull
+    private Integer grade;
+
+    @NotNull
+    private Integer classNum;
+
+    @NotNull
+    private Integer number;
+
+}
