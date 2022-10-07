aabe8b37622451ac14dd9a9dd6cf959fe490a4d0
KangMin <alsdl0629@dsm.hs.kr>
2022-10-07 18:51:04 +0900

    🗂 :: QueryFeedListResponse

---
+++ src/main/java/com/das/das_backend/domain/feed/presentation/dto/response/QueryFeedListResponse.java
@@ -0,0 +1,28 @@
+package com.das.das_backend.domain.feed.presentation.dto.response;
+
+import com.das.das_backend.domain.user.presentation.dto.response.WriterResponse;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@AllArgsConstructor
+public class QueryFeedListResponse {
+
+    private final List<FeedResponse> feedList;
+
+    @Getter
+    @Builder
+    public static class FeedResponse {
+        private final WriterResponse writer;
+        private final Integer feedId;
+        private final String title;
+        private final LocalDateTime createdAt;
+        private final Integer views;
+        private final Integer likeCounts;
+    }
+
+}
