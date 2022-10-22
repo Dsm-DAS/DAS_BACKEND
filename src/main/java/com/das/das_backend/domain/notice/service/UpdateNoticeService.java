24a43102605a0925e3ffa67940cdd71125f2ba99
Kangmin <ehfehf0629@gmail.com>
2022-10-23 00:25:17 +0900

    🗂 :: NoticeFacade

--- src/main/java/com/das/das_backend/domain/notice/service/UpdateNoticeService.java
+++ src/main/java/com/das/das_backend/domain/notice/service/UpdateNoticeService.java
@@ -1,8 +1,7 @@
 package com.das.das_backend.domain.notice.service;
 
 import com.das.das_backend.domain.notice.domain.Notice;
-import com.das.das_backend.domain.notice.domain.repository.NoticeRepository;
-import com.das.das_backend.domain.notice.exception.NoticeNotFoundException;
+import com.das.das_backend.domain.notice.facade.NoticeFacade;
 import com.das.das_backend.domain.notice.presentation.dto.request.UpdateNoticeRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -12,14 +11,13 @@ import org.springframework.transaction.annotation.Transactional;
 @Service
 public class UpdateNoticeService {
 
-    private final NoticeRepository noticeRepository;
+    private final NoticeFacade noticeFacade;
 
     @Transactional
     public void execute(UpdateNoticeRequest request, Integer noticeId) {
 
-        Notice notice = noticeRepository.findById(noticeId)
-                .orElseThrow(() -> NoticeNotFoundException.EXCEPTION);
-        
+        Notice notice = noticeFacade.getNoticeById(noticeId);
+
         notice.updateNotice(request.getTitle(), request.getContent());
     }
 
