e0d05d01e1faf64e91f57cd409d90e37a19996a5
Kangmin <ehfehf0629@gmail.com>
2022-10-28 23:41:30 +0900

    ♻️ :: import 제거

--- src/main/java/com/das/das_backend/domain/feed/domain/repository/FeedRepository.java
+++ src/main/java/com/das/das_backend/domain/feed/domain/repository/FeedRepository.java
@@ -1,7 +1,6 @@
 package com.das.das_backend.domain.feed.domain.repository;
 
 import com.das.das_backend.domain.feed.domain.Feed;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
