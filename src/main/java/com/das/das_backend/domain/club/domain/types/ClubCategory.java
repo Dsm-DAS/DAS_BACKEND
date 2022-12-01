27b5c888587073a3c4790dc35473611f23884c19
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-01 22:45:56 +0900

    🌧️ :: ENUM 안에 값 추가

--- src/main/java/com/das/das_backend/domain/club/domain/types/ClubCategory.java
+++ src/main/java/com/das/das_backend/domain/club/domain/types/ClubCategory.java
@@ -1,4 +1,5 @@
 package com.das.das_backend.domain.club.domain.types;
 
 public enum ClubCategory {
+    WEB, APP, EMBEDDED, GAME
 }
--- src/main/java/com/das/das_backend/domain/club/domain/types/ClubType.java
+++ src/main/java/com/das/das_backend/domain/club/domain/types/ClubType.java
@@ -1,4 +1,5 @@
 package com.das.das_backend.domain.club.domain.types;
 
 public enum ClubType {
+    MAIN, SUB, FREE
 }
