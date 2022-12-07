8c2f9866b989f41e515d82e87f4e17befd9f7f70
Kangmin <ehfehf0629@gmail.com>
2022-12-07 22:44:39 +0900

    🌧 :: 메소드 추가

--- src/main/java/com/das/das_backend/domain/notice/domain/repository/NoticeRepository.java
+++ src/main/java/com/das/das_backend/domain/notice/domain/repository/NoticeRepository.java
@@ -3,5 +3,9 @@ package com.das.das_backend.domain.notice.domain.repository;
 import com.das.das_backend.domain.notice.domain.Notice;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface NoticeRepository extends CrudRepository<Notice, Integer> {
+
+    List<Notice> findAll();
 }
