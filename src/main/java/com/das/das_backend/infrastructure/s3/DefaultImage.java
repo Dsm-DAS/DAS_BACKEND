4479dad07a51bf2a24c98caa45f5575706b0655c
jungho1209 <101508190+jungho1209@users.noreply.github.com>
2022-12-16 15:44:38 +0900

    Update DefaultImage.java

--- src/main/java/com/das/das_backend/infrastructure/s3/DefaultImage.java
+++ src/main/java/com/das/das_backend/infrastructure/s3/DefaultImage.java
@@ -6,7 +6,7 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class DefaultImage {
 
-    public static final String USER_PROFILE_IMAGE = "'https://test-imag-upload-bucket.s3.ap-northeast-2.amazonaws.com/image.png'";
+    public static final String USER_PROFILE_IMAGE = "https://test-imag-upload-bucket.s3.ap-northeast-2.amazonaws.com/image.png";
 
     public static final String CLUB_PROFILE_IMAGE = "'b'";
 
