b24d128c33b50360c3fae8848276ea1397d2370a
jungho1209 <dlwjdgh1209@dsm.hs.kr>
2022-12-16 23:37:42 +0900

    🌧️ :: printStackTrace 추가

--- src/main/java/com/das/das_backend/global/error/ExceptionFilter.java
+++ src/main/java/com/das/das_backend/global/error/ExceptionFilter.java
@@ -25,6 +25,7 @@ public class ExceptionFilter extends OncePerRequestFilter {
         } catch (DasException e) {
             sendErrorMessage(response, e.getErrorCode());
         } catch (Exception e) {
+            e.printStackTrace();
             sendErrorMessage(response, ErrorCode.INTERNAL_SERVER_ERROR);
         }
     }
