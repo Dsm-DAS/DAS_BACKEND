487f52490642cf582b2aa947d65556f2fc6e097c
Woonseok Yang <yws3786@gmail.com>
2022-12-16 16:31:08 +0900

    🌧 :: Club 삭제 추가

--- src/main/java/com/das/das_backend/domain/club/presentation/ClubController.java
+++ src/main/java/com/das/das_backend/domain/club/presentation/ClubController.java
@@ -3,6 +3,7 @@ package com.das.das_backend.domain.club.presentation;
 import com.das.das_backend.domain.club.presentation.dto.request.CreateClubRequest;
 import com.das.das_backend.domain.club.presentation.dto.request.UpdateClubRequest;
 import com.das.das_backend.domain.club.service.CreateClubService;
+import com.das.das_backend.domain.club.service.DeleteClubService;
 import com.das.das_backend.domain.club.service.UpdateClubService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +17,7 @@ public class ClubController {
 
     private final CreateClubService createClubService;
     private final UpdateClubService updateClubService;
+    private final DeleteClubService deleteClubService;
 
     @PostMapping
     public void createClub(@RequestBody @Valid CreateClubRequest request) {
@@ -26,6 +28,11 @@ public class ClubController {
     public void updateClub(@PathVariable("club-id") Integer clubId,
                            @RequestBody @Valid UpdateClubRequest request) {
         updateClubService.exceute(clubId, request);
+    }
+
+    @DeleteMapping("/{club-id}")
+    public void deleteClub(@PathVariable("club-id") Integer clubId) {
+        deleteClubService.execute(clubId);
     }
 
 }
